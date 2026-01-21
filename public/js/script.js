c45b37c7aeb7b6427d21225d9a42a616330653b3
plum-rh <riho.ham.plum@gmail.com>
2026-01-21 20:57:20 +0900

    upload202601212041

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,10 @@
 $(function () {
-  // 開閉
   $('.burger-btn').on('click', function () {
     $(this).toggleClass('cross');
     $('.sidebar').fadeToggle(300);
     $('body').toggleClass('noscroll');
   });
 
-  // メニュー項目クリックで閉じる（スマホ時のみ）
   $(document).on('click', '.sidebar a', function () {
     if (window.matchMedia('(max-width: 768px)').matches) {
       $('.burger-btn').removeClass('cross');
@@ -15,7 +13,6 @@ $(function () {
     }
   });
 
-  // PC幅に戻したら強制復帰（display:none のinline style対策）
   $(window).on('resize', function () {
     if (window.matchMedia('(min-width: 769px)').matches) {
       $('.sidebar').show().removeAttr('style');
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -396,7 +396,33 @@ body {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-<script src="{{ asset('js/script.js') }}"></script>
+<script>
+$(function () {
+  $('.burger-btn').on('click', function () {
+    $(this).toggleClass('cross');
+    $('.sidebar').fadeToggle(300);
+    $('body').toggleClass('noscroll');
+  });
+
+  $(document).on('click', '.sidebar a', function () {
+    if (window.matchMedia('(max-width: 768px)').matches) {
+      $('.burger-btn').removeClass('cross');
+      $('.sidebar').fadeOut(300);
+      $('body').removeClass('noscroll');
+    }
+  });
+
+  $(window).on('resize', function () {
+    if (window.matchMedia('(min-width: 769px)').matches) {
+      $('.sidebar').show().removeAttr('style');
+      $('.burger-btn').removeClass('cross');
+      $('body').removeClass('noscroll');
+    }
+  });
+});
+
+
+</script>
 
 </body>
 </html>
